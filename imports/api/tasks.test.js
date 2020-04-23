d44485a390979bc4ee329ca1724c3a463e5bc2dd
Ramakrishnan CS <=>
2020-04-23 19:12:58 +0530

    Added tests

--- imports/api/tasks.test.js
+++ imports/api/tasks.test.js
@@ -7,8 +7,14 @@ import { Tasks } from './tasks.js';
 if (Meteor.isServer) {
   describe('Tasks', () => {
     describe('methods', () => {
-      const userId = Random.id();
+      let userId;
       let taskId;
+      let taskCount;
+
+      before(() => {
+        Meteor.users.remove({ username: 'tester' });
+        userId = Meteor.users.insert({ username: 'tester' });
+      });
  
       beforeEach(() => {
         Tasks.remove({});
@@ -18,6 +24,7 @@ if (Meteor.isServer) {
           owner: userId,
           username: 'tester',
         });
+        taskCount = Tasks.find({}).count();
       });
  
       it('can delete owned task', () => {
@@ -28,6 +35,37 @@ if (Meteor.isServer) {
         
         assert.equal(Tasks.find().count(), 0);
       });
+
+      it('can add a task', () => {
+        const insertTask = Meteor.server.method_handlers['tasks.insert'];
+        const invocation = { userId };
+        const payload = 'test task';
+        
+        insertTask.apply(invocation, [payload]);
+        
+        assert.equal(Tasks.find().count(), taskCount + 1);
+      });
+
+      it('anonymous user cannot add a task', () => {
+        const insertTask = Meteor.server.method_handlers['tasks.insert'];
+        const invocation = {};
+        const payload = 'test task';
+        
+        const insertAction = () => insertTask.apply(invocation, [payload]);
+        
+        assert.throws(insertAction, Error, 'not-authorized');
+      });
+
+      it('can setChecked to a task', () => {
+        const setCheckedFn = Meteor.server.method_handlers['tasks.setChecked'];
+        const invocation = { userId };
+        const payload = true;
+        
+        setCheckedFn.apply(invocation, [taskId, payload]);
+        
+        assert.equal(Tasks.findOne({ _id: taskId }).checked, true);
+      });
+
     });
   });
 }
